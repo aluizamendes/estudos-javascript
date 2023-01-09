1b6bd99b43a35decab8166f42f1c4789d841b96d
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-01-09 01:06:12 -0300

    aula 7

---
+++ 05_Curso_JS_CursoEmVideo/aula07/ex002/ex02.js
@@ -0,0 +1,7 @@
+let valores = [8, 1, 7, 4, 2, 9];
+
+// percorrendo um vetor
+for (i = 0; i < valores.length; i++) {
+  console.log(`A posição ${i} tem valor ${valores[i]}`);
+}
+
---
+++ 05_Curso_JS_CursoEmVideo/aula07/ex003/ex03.js
@@ -0,0 +1,5 @@
+let valores = [8, 1, 7, 4, 2, 9];
+
+for (let i in valores) {
+  console.log(`A posição ${i} tem valor ${valores[i]}`)
+}
